d51a70eac8346f22197d484bdc9682e980a4fc47
kishorekumarxyz <83898279+kishorekumarxyz@users.noreply.github.com>
2023-06-13 12:29:58 +0530

    its not working
    unable to read data from the clipboard items.

--- main.ts
+++ main.ts
@@ -1,137 +1,222 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	TFile,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting
+} from 'obsidian';
+
+
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface SvgPastePluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: SvgPastePluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class SvgPastePlugin extends Plugin {
+	settings: SvgPastePluginSettings;
 	async onload() {
-		await this.loadSettings();
+		console.log('loading SVGPaste plugin');
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// register event for paste event and call handlePaste
+		this.registerEvent(this.app.workspace.on("editor-paste", this.handlePaste.bind(this)));
 	}
 
 	onunload() {
+		console.log('unloading SVGPaste plugin');
+	}
+
+	async handlePaste(event: ClipboardEvent, editor: Editor, mdView: MarkdownView): Promise<void> {
+		console.log("Pasted something");
+		console.log(event);
+
+		if (event.defaultPrevented || !event.clipboardData) return;
+		//console.log(event.clipboardData.items);
+
+		//get svg data
+		let svgData = "";
+
+		// ---- Approach 1: get svg data from event.clipboardData.items ----
+		const items = Array.from(event.clipboardData.items);
+		const svgItem = items.find(item => item.type.includes("image/svg"));
+		//items.forEach(item => console.log('mime-type:' + item.type));
+		if (svgItem) {
+			//console.log(svgItem);
+			console.log("found svg in: " + svgItem.type);
+			event.preventDefault();
+
+
+			//method1: save sampleSvg using string method
+			svgItem.getAsString(
+				(data) => {
+					console.log('getAsString method:' + data.length); //<--- ⚠ output: 0
+				});
+
+			//method2: get svgData using blob method
+			var blob = svgItem.getAsFile();
+			console.log('blob method:' + blob) //<---  output: ⚠  `null`
+			if (blob) {
+				const reader = new FileReader();
+				reader.onloadend = () => {
+					svgData = reader.result as string;
+					console.log('onloadend' + svgData);
+
+				};
+				reader.readAsText(blob);
+			}
+		}
+
+		// ---- Approach 2: get svg data from window.navigator. ----
+
+		//method3: get svgData using navigator.readText method <-- how to make sure its only svg?
+		activeWindow.navigator.clipboard.readText().then(
+			(clipText) => {
+				svgData = clipText;
+				console.log('navigator.clipboard:' + svgData);
+			}
+		);
+
+		//method4: get svgData using navigator.read method
+		try {
+			const items = await navigator.clipboard.read();
+			const svgItem = items.find(item => item.types.includes("image/svg"));
+
+			if (svgItem) {
+				const blob = await svgItem.getType("image/svg+xml");
+
+				const reader = new FileReader();
+				reader.onloadend = () => {
+					const svgData = reader.result as string;
+					console.log('navigator.read:', svgData);
+				};
+
+				reader.readAsText(blob);
+			}
+		} catch (error) {
+			console.error("Failed to read clipboard data:", error);
+		}
+
+		//save svg file & insert the new svg file link in the editor
+		const svgName = this.getRandomSvgName();
+		//console.log(svgName);
+		this.saveSvgFile(svgName, svgData);
+		//insert svg file link
+		const svgFileLink = `![[${svgName}]]`;
+		editor.replaceSelection(svgFileLink);
+
 
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+
+	async isPasteSvg() {
+		try {
+			// read clipboard items 
+			// and iterate through items to find 'format' of each item 
+			// & get data for svg format
+
+
+		}
+		catch (error) {
+			console.error("Error reading clipboard data:", error);
+		}
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+	getRandomSvgName(): string {
+		// random string of 11 characters containing a-zA-Z0-9
+		let length = 11;
+		let randomString = '';
+		const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+		const charactersLength = characters.length;
+
+		for (let i = 0; i < length; i++) {
+			const randomIndex = Math.floor(Math.random() * charactersLength);
+			randomString += characters.charAt(randomIndex);
+		}
+
+		return `svg_${randomString}.svg`;
+
+	}
+
+	isTextSvg(data: string | Blob, type: string): boolean {
+
+		/* 
+		<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+		<!-- Created with Inkscape (http://www.inkscape.org/) -->
+
+		<svg
+		version="1.1"
+		id="svg29731"
+		xml:space="preserve"
+		*/
+		//does beging with <?xml version="1.0" 
+		if (data.startsWith("<?xml version=\"1.0\"") && data.includes("<svg")) {
+			return true;
+		}
+		return false;
+	}
+
+	saveSvgFile(filename: string, data: string): void {
+
+		//get current folder path
+		const activeFile: TFile | null = this.app.workspace.getActiveFile();
+		if (!activeFile) {
+			// throw error
+			new Notice("No active file");
+			return;
+		}
+		const currentFolderPath: string | null = activeFile.parent.path;
+
+		//convert string to arraybuffer
+		const svgArrayBuffer = new TextEncoder().encode(data).buffer;
+		//create file in the current folder
+		this.app.vault.createBinary(`${currentFolderPath}/${filename}`, svgArrayBuffer);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+
+export class FileNameModal extends Modal {
+	result: string;
+	onSubmit: (result: string) => void;
+
+	constructor(app: App, onSubmit: (result: string) => void) {
 		super(app);
+		this.onSubmit = onSubmit;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+
+		contentEl.createEl("h1", { text: "FileName" });
+
+		new Setting(contentEl)
+			.setName("Name")
+			.addText((text) =>
+				text.onChange((value) => {
+					this.result = value
+				}));
+
+		new Setting(contentEl)
+			.addButton((btn) =>
+				btn
+					.setButtonText("Submit")
+					.setCta()
+					.onClick(() => {
+						this.close();
+						this.onSubmit(this.result);
+					}));
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		let { contentEl } = this;
 		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
